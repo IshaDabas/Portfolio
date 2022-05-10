aaa145abef684adcd5efeb0b16594446b809e5a2
IshaDabas <ishadabas@icloud.com>
2022-05-10 17:20:51 +0530

    completed portfolio

---
+++ Assignment-1/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Footer Page</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <link rel="stylesheet" href="css/footer.css" />
+</head>
+<html>
+<body>
+<footer>
+<div class="footer-content">
+   <h3>Isha Dabas</h3>
+   <p>A Freelancer Frontend Developer and Graphic Designer.
+   <q>Expect the unexpected.</q> Hardwork will give you unexpecting results. </p>
+</div>
+<ul class="socials">
+    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+    <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+    <li><a href="https://github.com/IshaDabas"><i class="fa-brands fa-github"></i></a></li>
+ </ul>
+ <div class="footer-bottom">
+    <p>copyright &copy;2022 | Frontenddeveloper </p>
+</div>
+</footer>
+</body>
+</html>
